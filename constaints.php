9606c8b56cfb92950c59110534002725998c435a
吴昌敏 <ckeen.wu@gmail.com>
2015-07-14 18:00:53 +0800

    Update constaints.php

--- constaints.php
+++ constaints.php
@@ -1,8 +1,6 @@
 <?php
 	define('TOKEN','carketest');
-	define('MAIN_APPID','wx6ac360c863090a23');
 	define('TEST_APPID','wx911a7106428aedd1');
-	define('MAIN_APPSECRET','77abb8825f9ce11c6c96785ec6d706a5');
 	define('TEST_APPSECRET','3ab0dee3c87c2368ad89c57abdcac04d');
 
 	//获取access_token的方法
